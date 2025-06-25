e6e947b964c37027d764fb1281a6fba0ef4a5c01
Princia-dev <oguerep@gmail.com>
2025-06-25 13:16:36 +0100

    backend.py

--- serveur/backend.py
+++ serveur/backend.py
@@ -14,7 +14,7 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 BASE_URL = os.getenv("BASE_URL", "http://localhost:8000")
 
 DB_CONFIG = {
-    "host": "https://tracksys-backend.onrender.com",
+    "host": "tracksys_bd.onrender.com",
     "dbname": "tracksys_bd",
     "user": "postgres",
     "password": "Boucledor",
